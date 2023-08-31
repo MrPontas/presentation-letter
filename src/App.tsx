28259c6765907b4a17d1696c76f6c53396b51dc2
Guilherme Pontarolo <guilherme.pontarolo@viewsoftwarehouse.com>
2023-08-31 15:02:31 -0300

    refac: change info to my personal data

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,9 @@ function App() {
         <div className="info">
           <div className="icon-with-text">
             <PersonOutline sx={{ color: "white" }} />
-            <p style={{ fontWeight: "bold", fontSize: 20 }}>Paola Schnaider</p>
+            <p style={{ fontWeight: "bold", fontSize: 20 }}>
+              Guilherme Pontarolo
+            </p>
           </div>
           <div>
             <div className="icon-with-text">
@@ -22,41 +24,28 @@ function App() {
               <div className="address">
                 <p>Rua Theorodo Rosas, 159 - Centro</p>
                 <p style={{ fontWeight: 400, color: "#e9e9e9" }}>
-                  Ponta Grossa - PR, 84010-180
+                  Ponta Grossa - PR, 84010-180 - Brazil
                 </p>
               </div>
             </div>
           </div>
           <div className="icon-with-text">
             <WhatsApp sx={{ color: "white" }} />
-            <p>(42) 9 9910-3267</p>
+            <p>+55 (42) 9 3618-0803</p>
           </div>
           <div className="icon-with-text">
             <MailOutline sx={{ color: "white" }} />
-            <p>paoladefatima12@gmail.com</p>
+            <p>guipontarolo@gmail.com</p>
           </div>
         </div>
         <div className="presentation">
-          <h1>Estágio em administração</h1>
+          <h1>Frontend Dev</h1>
           <div className="text-container">
             <p>
-              Meu nome é Paola de Fátima Schnaider, tenho 22 anos e sou formada
-              em Administração pelo curso técnico, e atualmente, estou cursando
-              Bacharelado em Administração.
-            </p>
-            <p>
-              Eu adquiri conhecimentos técnicos e práticos em administração
-              durante meu curso técnico e, agora, estou aprimorando ainda mais
-              essas habilidades com o curso de bacharelado. Além disso, estou
-              sempre buscando novos desafios e aprimoramento de minhas
-              habilidades.
-            </p>
-            <p>
-              Sou uma pessoa organizada, comprometida e com grande capacidade de
-              comunicação, o que me permite trabalhar em equipe e lidar com
-              pessoas de diferentes perfis e culturas. Possuo conhecimentos em
-              gerenciamento de estoque, atendimento ao cliente, gestão de
-              documentos e processos administrativos.
+              Hi! I'm computer engineer, and I'm passionated about UX/UI. I also
+              love programming, which led me to focus my career on the front
+              end. I am looking for a job opportunity to help me improve my
+              career, by doing what I do best.
             </p>
           </div>
         </div>
